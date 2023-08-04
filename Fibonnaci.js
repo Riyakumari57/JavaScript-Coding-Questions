8df58d49a7bd664f2b131ceae6281c83108102cd
Riya <riiyaasinghh@gmail.com>
2023-08-04 13:30:37 +0530

    Fibonnaci.js

--- Fibonnaci.js
+++ Fibonnaci.js
@@ -1,1 +1,36 @@
-//Fibonacci series
+//Fibonacci 
+
+
+//Without Recursion
+let f0 = 0;
+let f1  = 1;
+console.log(f0);
+console.log(f1);
+for(let i = 0 ; i<10 ;i++)
+{
+    let fi = f1 + f0;
+    console.log(fi);
+    f0 = f1;
+    f1 = fi;
+}
+
+
+//with recursion
+console.log("nth fibonacci using recursion");
+function findFinbonnaci(n)
+{
+    if(n==0) return 0;
+    if(n==1) return 1;
+
+    return findFinbonnaci(n-1)+findFinbonnaci(n-2);
+}
+
+console.log(findFinbonnaci(6));
+
+
+
+
+
+
+
+
