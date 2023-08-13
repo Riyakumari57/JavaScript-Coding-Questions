611a6377f7600f6581354bdcd74f90bc8374a823
Ashwani Kumar <119940023+AshwinKumar0@users.noreply.github.com>
2023-08-13 13:34:07 +0530

    Optimized Prime Check.js
     When we want to check if a number is prime (can only be divided by 1 and itself), we only need to look for possible divisors up to the square root of that number. Why? Because if a number has a larger divisor, it would also have a smaller one, and we would have already checked that. Checking only up to the square root saves time and still gives us the same answer.
    
    Example:-
    n = 36
    
    Factors of 36:
    1 × 36 = 36
    2 × 18 = 36
    3 × 12 = 36
    4 × 9 = 36
    6 × 6 = 36
    9 x  4 = 36
    12 x 3 = 36
    18 x 2 = 36
    36 x 1 = 36
    
    So as you can see after 6 which is Square root of 36 the factors are repeating itself.

--- problems/Problem5.js
+++ problems/Problem5.js
@@ -1,14 +1,20 @@
 //n prime numbers
 
 
-function isPrime(n)
-{
-    if(n<2) return false;
-    if(n==2) return true;
-    for(let i = 2 ; i<n ; i++)
-    {
-        if(n%i === 0)
-        return false
+function isPrime(n) {
+    if (n < 2) {
+        return false;
+    }
+    if (n === 2) {
+        return true;
+    }
+    if (n % 2 === 0) { // if its greater then 2 and even no need to go further.
+        return false;
+    }
+    for (let i = 3; i * i <= n; i += 2) { //i*i <= sqrt(n);
+        if (n % i === 0) {
+            return false;
+        }
     }
     return true;
 }
