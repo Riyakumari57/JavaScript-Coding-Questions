8da81269a4589321021b52aa24acffd82182a953
Riya Kumari <riiyaasinghh@gmail.com>
2023-08-15 21:50:27 +0530

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -1,20 +1,6 @@
 let arr = [2 , 3 , 4,5];
-function sumArray(total  , num)
-{ 
-    console.log(`total is ${total}`);
-   return total +num;
-}
+const sumArray =  arr.reduce((total  , num) =>{
+    return total+num;
+},0);
 
-// function RoundSum(total , num)
-// {
-   
-//     return total+ Math.round(num);
-// }
-function getOutput() {  
-   console.log( arr.reduce(sumArray));
-    // console.log(arr.reduce(RoundSum));
-}
-
-
-
-getOutput();
+console.log(sumArray);
