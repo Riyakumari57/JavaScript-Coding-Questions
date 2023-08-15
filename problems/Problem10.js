5599ccd6add94d41e09fa8eaf9cab178a16a81ad
Riya Kumari <riiyaasinghh@gmail.com>
2023-08-16 00:06:26 +0530

    Update Problem10.js
    Merge Two Sorted array

--- problems/Problem10.js
+++ problems/Problem10.js
@@ -1,7 +1,12 @@
 // Merge two sorted array 
-const arr1 = [8 , 9 , 10 , 11 ];
-const arr2 = [2 , 3 , 4, 5 ];
+const arr1 = [8 , 9 , 9, 10 , 11 ];
+const arr2 = [2 , 3 , 4, 4,  5 ];
 
 let arr3 = [...arr1 , ...arr2];
 arr3.sort((a, b) => a - b);
+console.log(arr3);
+
+// after removing duplicates
+
+arr3 = [... new Set(arr3)];
 console.log(arr3);
