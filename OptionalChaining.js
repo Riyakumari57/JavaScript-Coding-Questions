62156f2e5ec52a2b0ad18742446adaba6ed3ed35
Riya Kumari <riiyaasinghh@gmail.com>
2023-08-16 13:32:08 +0530

    Create OptionalChaining.js

---
+++ OptionalChaining.js
@@ -0,0 +1,14 @@
+const user = {
+    name :"Riya Kumari" ,
+    age: 20 ,
+    address:{
+        street:" Road ",
+        city : "Patna" ,
+        state : "Bihar",
+        zip : 4657890
+        
+    }
+    
+}
+
+console.log(user.address.country?.pin); //undefined
