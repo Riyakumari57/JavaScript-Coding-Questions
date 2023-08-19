389c7be69243f66b97106fd1c4e4710d842718ab
Riya Kumari <riiyaasinghh@gmail.com>
2023-08-19 15:59:06 +0530

    Update ReverseString.js

--- ReverseString.js
+++ ReverseString.js
@@ -2,14 +2,10 @@
 
    let reverseEntireSentence =  reverseBySeparator(string, "");
    let reverseEachWord = reverseBySeparator(reverseEntireSentence, " ");
-/* As a general rule, you should always declare variables with const,
-   if you realize that the value of the variable needs to change, go back and change it to let.
-   Use let when you know that the value of a variable will change.
-   Use const for every other variable. Do not use var.*/
 
-console.log(reverseEntireSentence);
-console.log(reverseEachWord);
+   console.log(reverseEntireSentence);
+   console.log(reverseEachWord);
 
-function reverseBySeparator(string, separator) {
+   function reverseBySeparator(string, separator) {
     return string.split(separator).reverse().join(separator);
-  }
+   }
